616da51e306786df730ebeaaa1660df4bcb51298
el-j <bluearc.in.sky@gmail.com>
2019-01-23 12:32:49 +0100

    write sketchobjects as files and have a checking process

--- index.js
+++ index.js
@@ -28,35 +28,82 @@ let filename = 'test.fzz'
 
 const dataBaseFolder = './examples'
 const dataBase = []
+let newTemp = {}
 
 const loadDataBase = (res) => fs.readdir(dataBaseFolder, (err, files) => {
+  console.log(files);
   files.forEach((file,key) => {
-		let temp = path.parse(file).name
-
-		if (
-			_.find(dataBase, function(el) {
-			return el === temp}))
+		let temp = {name:path.parse(file).name, ext:path.parse(file).ext, data: {} }
+    // console.log("this is temp",temp);
+		if (_.find(dataBase,el => el.name === temp.name))
 			{
-			console.log('Known:', file);
+          console.log('we have it in database',temp.name);
+        _.find(dataBase, function(el, key) {
+          if (el.name === temp.name) {
+              // console.log('database is empty',dataBase[key],el,temp);
+              if (_.isEmpty(el.data)) {
+                // console.log('before extension check',temp);
+            if (temp.ext === '.json') {
+              console.log('have a json for that',temp.name, temp.ext);
+              let thePath = './examples/' + temp.name + temp.ext
+              console.log(thePath);
+                fs.readFile(thePath ,function(err,data){
+                  // console.log('storing',key, dataBase[key],data);
+                newTemp = JSON.parse(data)
+                // console.log('OUR NEW TEMP FROM READFILE',newTemp);
+                dataBase[key].data = newTemp
+                // console.log('NEW TEMP OUTSIDE READFILE',dataBase)
+                  })
+                  // console.log('the element', dataBase);
+                  // dataBase.push(temp)
+            }
+          }
+
+        return el === temp}}
+      )
+			console.log('Known:',dataBase, file);
 		}
 
 		else {
-			console.log('NEW FILE:', temp);
-			dataBase.push(temp)
+      if (temp.ext !== '.json') {
+        dataBase.push(temp)
+        console.log('NEW FILE:', temp);
+
+      }
 		}
   });
 	res.json(dataBase);
 })
 
 function download(filename,res) {
+
+    let dbDataName = filename.slice(0, -4)
+    console.log(dbDataName);
+    if (_.find(dataBase,el =>
+      {
+        if (!_.isEmpty(el.data)){
+        return el.name === dbDataName}
+      }
+      )){
+        _.find(dataBase,el => {
+          (el.name === dbDataName) ? (
+            console.log("the data we use",el),
+            res.json(el.data)):null
+          })
+        console.log('USE FROM DATABASE',dbDataName)
+      }
+
+    else {
+      console.log('READ NEW FILE',filename)
+
 		return fs.readFile('./examples/'+filename,function(err,data){
 			if (data) {
 					SketchBundle.fromFZZ(data)
 						.then((sketchBundle) => {
 							console.log("from sketchBundle", filename,filename.slice(0, -1));
 							let mytest = sketchBundle.primary
-							filename = filename.slice(0, -1)
-							let myInstances = sketchBundle.primary[filename].instances
+							fzFilename = filename.slice(0, -1)
+							let myInstances = sketchBundle.primary[fzFilename].instances
 							/*
 							get all the parts that are not wires
 							*/
@@ -76,10 +123,11 @@ function download(filename,res) {
 							get the arduino instance
 							*/
 							let newArduino = _.find(iLoveTest, function(el) {
- 								if (el.name.includes('arduino')){
+ 								if (el.name.includes('arduino') && !el.name.includes('shield')){
  								return el
  								}
  							})
+              console.log("we have a new Arduino found",newArduino);
 						  arduinoConnected = []
 							 myConnections.mainBoardEndConnection.map(connector => {
 								 arduinoConnected.push({connector:connector.connections[0].startId, type: '',connected: true, breadSvgId: ''})
@@ -107,36 +155,56 @@ function download(filename,res) {
                      })
                      // console.log("this is my temps from what i have found",temp);
                      if (temp) {
-                       console.log("this is the temp",temp.views.breadboard.FZPConnectorView, temp.views.breadboard.svgId);
+                       console.log("this is the temp",temp, temp.views.breadboard.svgId);
 
                      switch (true) {
                        case temp.name.includes('gnd') || temp.name.includes('GND') :
                           console.log("a ground connector");
                           returnObject.connected[key].type = 'GND'
+                          returnObject.connected[key].name = temp.name
                           returnObject.connected[key].breadSvgId = temp.views.breadboard.svgId
                          break;
 
                        case temp.name.includes('5V') || temp.name.includes('3v3') || temp.name.includes('3V') :
                           console.log("a ground connector");
                           returnObject.connected[key].type = 'V'
+                              returnObject.connected[key].name = temp.name
                           returnObject.connected[key].breadSvgId = temp.views.breadboard.svgId
                          break;
 
                        case temp.name.includes('A'):
                        console.log("an analog connector");
-                       returnObject.connected[key].type = 'in'
+                       returnObject.connected[key].type = 'out'
+                           returnObject.connected[key].name = temp.name
                        returnObject.connected[key].breadSvgId = temp.views.breadboard.svgId
                        break;
 
                        default:
                          returnObject.connected[key].type = 'unknown'
+                             returnObject.connected[key].name = temp.name
                          returnObject.connected[key].breadSvgId = temp.views.breadboard.svgId
                      }
                      // temp.name
                    }
                    })
+                   jsonFilename = filename.slice(0, -4)
+
+                   console.log('the jsonFilename', jsonFilename);
+                   _.find(dataBase,el => {
+                       if (el.name === jsonFilename){
+                           if (_.isEmpty(el.data)) {
+                             el.data = returnObject
+                             fs.writeFileSync('./examples/'+jsonFilename+'.json', JSON.stringify(returnObject))
+                             return
+                           }
+                          }
+                         else {
+                        fs.writeFileSync('./examples/'+jsonFilename+'.json', JSON.stringify(returnObject))
+                      }
+                       return el.name === filename
+                    })
                    // console.log(returnObject);
-						        fs.writeFileSync('./data/return.json', JSON.stringify(returnObject));
+
 							 	 res.json(returnObject);
 							 })
 							 .catch((err) => {
@@ -161,6 +229,7 @@ function download(filename,res) {
 		})
 		}
 	})
+}
 }
 
 
